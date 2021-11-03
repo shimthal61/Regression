009ee6db45b7aa47748dfdd70cdc638d6e45c3d4
Harvey Schneider <50023408+shimthal61@users.noreply.github.com>
2021-11-03 20:19:17 +0000

    Created new plot with filtered data

--- Regression 1/Visualisation_and_analysis.R
+++ Regression 1/Visualisation_and_analysis.R
@@ -17,3 +17,15 @@ rcorr(crime_tidy$Population, crime_tidy$Violent_Crimes)
 print(0.81*0.81)*100
 #Therefore, around 66% of the variance in our Violent_Crimes variable is explained by population size
 
+crime_filtered <- crime_tidy %>% 
+  filter(Population < 2000000)
+
+crime_filtered %>% 
+  ggplot(aes(x = Population, y = Violent_Crimes)) +
+  geom_point(alpha = .25) +
+  geom_smooth(method = "lm", se = FALSE) +
+  theme_minimal() +
+  theme(text = element_text(size = 13)) +
+  labs(title = "Examining the Effect of Population on Violent Crimes",
+       x = "Population",
+       y = "Violent Crime")
