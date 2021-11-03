1ddba108101ed38702c461509bd30ed333e4bf86
Harvey Schneider <50023408+shimthal61@users.noreply.github.com>
2021-11-03 20:14:52 +0000

    Calculating Pearson's r

--- Regression 1/Visualisation_and_analysis.R
+++ Regression 1/Visualisation_and_analysis.R
@@ -7,3 +7,13 @@ crime_tidy %>%
   labs(title = "Examining the Effect of Population of Violent Crimes",
        x = "Population",
        y = "Violent Crimes")
+
+#We can carry out Pearson's r
+rcorr(crime_tidy$Population, crime_tidy$Violent_Crimes)
+#Looking at the top, our r value is 0.81
+#Looking at the bottom, our p value is 0 (i.e. p < 0.001.)
+
+#Let's calculate our r^2 value
+print(0.81*0.81)*100
+#Therefore, around 66% of the variance in our Violent_Crimes variable is explained by population size
+
